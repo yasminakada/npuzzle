5e108623f7618f0051bc1f2f69fee4d1d9faacde
yasminakada <y.kada@live.nl>
2014-12-11 21:42:47 +0100

    Refactoring: Commented everything!

--- NPuzzle10001567/app/src/main/java/projects/mprog/nl/npuzzle10001567/BitmapConstruct.java
+++ NPuzzle10001567/app/src/main/java/projects/mprog/nl/npuzzle10001567/BitmapConstruct.java
@@ -12,21 +12,26 @@ import android.view.Display;
 /**
  * Created by yasmina on 3-12-2014.
  */
+
 public class BitmapConstruct {
+    // Calculates dp to px.
     public static int dpToPx(int dp) {
         return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
     }
-
+    // Calculates px to dp.
     public static int pxToDp(int px) {
         return (int) (px / Resources.getSystem().getDisplayMetrics().density);
     }
 
-
+    // Scales bitmap to a square with given dimensions.
     public static Bitmap scaledBitmap(Bitmap bmp, int imgDim) {
+
         int h = bmp.getHeight();
         int w = bmp.getWidth();
         int startx = 0;
         int starty = 0;
+
+        // Image is made square by cutting of a part of the longest side, evenly.
         if (h > w) {
             starty = (int) (h - w) / 2;
             h = w;
@@ -39,36 +44,35 @@ public class BitmapConstruct {
         return bmp;
     }
 
+    // Calculates inSampleSize that should be used to decode a resource image efficiently.
     public static int calculateInSampleSize(
-            BitmapFactory.Options options, int reqDim) {
+
+        BitmapFactory.Options options, int reqDim) {
         // Raw height and width of image
         final int height = options.outHeight;
         final int width = options.outWidth;
         int inSampleSize = 1;
         int dim;
 
+        // The shortest side is used for deciding the dimensions
+        // This is because the image will most likely be cut later on.
         if (height >= width){
             dim = width;
         }else{
             dim = height;
         }
-
         if (dim > reqDim) {
-
-//            final int halfHeight = height / 2;
-//            final int halfWidth = width / 2;
             final int halfDim = dim /2;
-
             // Calculate the largest inSampleSize value that is a power of 2 and keeps both
             // height and width larger than the requested height and width.
             while ((halfDim / inSampleSize) > reqDim) {
                 inSampleSize *= 2;
             }
         }
-
         return inSampleSize;
     }
 
+    // Load a image efficiently.
     public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId, int reqDim) {
 
         // First decode with inJustDecodeBounds=true to check dimensions
